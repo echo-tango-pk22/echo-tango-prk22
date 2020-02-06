09a1b841b36fb3cd363c4d521dbdee9ae85dde85
echo-tango-pk22 <60750461+echo-tango-pk22@users.noreply.github.com>
2020-02-06 14:11:53 -0500

    Update nc-u18.04-install.sh

--- nc-u18.04-install.sh
+++ nc-u18.04-install.sh
@@ -16,7 +16,7 @@ sudo snap install nextcloud
 echo "Sit back and relax :) ......"
 sleep 2;
 cd /etc/nginx/sites-available/
-sudo wget -O "$DOMAIN" https://raw.githubusercontent.com/echo-tango-pk22/echo-tango-prk22/master/nginx.conf?token=AOPPU7KYGQPQ2FYDYXHJSE26HREDE
+sudo wget -O "$DOMAIN" https://raw.githubusercontent.com/echo-tango-pk22/echo-tango-prk22/master/nginx.conf
 sudo sed -i -e "s/example.com/$DOMAIN/" "$DOMAIN"
 sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/
 
